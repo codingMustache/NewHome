895f8cce095912fbde7879989143c5c9f70f28f0
zoekhall <zoe.k.hall@gmail.com>
2022-11-17 22:32:57 -0600

    rendering result

--- client/src/components/V2/Quiz/Questionaire/Questionaire.jsx
+++ client/src/components/V2/Quiz/Questionaire/Questionaire.jsx
@@ -2,15 +2,13 @@ import React, { useState } from 'react';
 import Question from './Question.jsx';
 import questions from './quizData.js';
 
-function Questionaire({ setView }) {
-  const answers = {
-    dog: 0,
-    cat: 0,
-    noPets: 0,
-    rabbit: 0,
-  };
+function Questionaire({ setView, setResult }) {
+  const [noPetsCount, setNoPetsCount] = useState(0);
+  const [dogCount, setDogCount] = useState(0);
+  const [catCount, setCatCount] = useState(0);
+  const [rabbitCount, setRabbitCount] = useState(0);
   const [currentQuestion, setCurrentQuestion] = useState(0); // current question rendered
-  const [answer, setAnswer] = React.useState(''); // set answer
+  const [answer, setAnswer] = useState(''); // set answer
 
   const handleChange = (event) => {
     setAnswer(event.target.value);
@@ -18,14 +16,29 @@ function Questionaire({ setView }) {
 
   const handleClick = () => {
     // handle count
-    // console.log(answer);
-    // console.log(answers[answer]);
-    // answers[answer] += 1;
+    if (answer === 'dog') {
+      setDogCount(dogCount + 1);
+    } else if (answer === 'cat') {
+      setCatCount(catCount + 1);
+    } else if (answer === 'rabbit') {
+      setRabbitCount(rabbitCount + 1);
+    } else if (answer === 'noPets') {
+      setNoPetsCount(noPetsCount + 1);
+    }
     // // handle next question
     const nextQuestion = currentQuestion + 1;
     if (nextQuestion < questions.length) {
       setCurrentQuestion(nextQuestion);
     } else {
+      const answers = [
+        { type: 'dog', count: dogCount },
+        { type: 'cat', count: catCount },
+        { type: 'rabbit', count: rabbitCount },
+        { type: 'noPets', count: noPetsCount },
+      ];
+
+      const sortAnswers = answers.sort((a, b) => b.count - a.count);
+      setResult(sortAnswers[0].type);
       setView('result');
     }
   };
